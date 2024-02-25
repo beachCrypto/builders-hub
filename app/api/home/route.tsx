95dd9fff2352053c76dbbc1807651d73098713cd
Beach Crypto <beachcrypto1@gmail.com>
2024-02-25 00:11:05 -0800

    Updating some files

--- app/api/home/route.tsx
+++ app/api/home/route.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from 'next/og';
+import { ImageResponse } from 'next/og.js';
 
 export const runtime = 'edge';
 
--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from 'next/og';
+import { ImageResponse } from 'next/og.js';
 // App router includes @vercel/og.
 // No need to install it.
 
