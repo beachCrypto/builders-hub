af193dde26517d4553806c8d05544ca38e9db863
Beach Crypto <beachcrypto1@gmail.com>
2024-02-24 23:39:35 -0800

    Working with local fonts

--- app/api/home/route.tsx
+++ app/api/home/route.tsx
@@ -3,27 +3,36 @@ import { ImageResponse } from 'next/og';
 export const runtime = 'edge';
 
 export async function GET() {
+  const fontData = await fetch(
+    'https://github.com/beachCrypto/builders-hub/blob/380e00fda4740926314698b498df6a84fb682b7d/SartoshiScript-Regular.otf',
+  ).then((res) => res.arrayBuffer());
+
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 40,
-          color: 'black',
-          background: 'white',
-          width: '100%',
+          backgroundColor: 'white',
           height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          alignItems: 'center',
+          width: '100%',
+          fontSize: 100,
+          fontFamily: '"SartoshiScript"',
+          paddingTop: '100px',
+          paddingLeft: '50px',
         }}
       >
-        👋 Hello
+        Hello world!
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: 'SartoshiScript',
+          data: fontData,
+          style: 'normal',
+        },
+      ],
     },
   );
 }
