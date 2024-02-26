a3ec005dd585f2ba8da7cec2095bc37a69d319fb
Beach Crypto <beachcrypto1@gmail.com>
2024-02-25 16:41:47 -0800

    we are all mfers

--- app/frames/mferTest/api/frame/route.ts
+++ app/frames/mferTest/api/frame/route.ts
@@ -9,137 +9,16 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
-  const imageUrl1 = `${NEXT_PUBLIC_URL}/frames/mferTest/api/images/alwaysHaveBeen`;
-  const imageUrl2 = `${NEXT_PUBLIC_URL}/frames/mferTest/api/images/everyoneIsAMfer`;
-  const imageUrl3 = `${NEXT_PUBLIC_URL}/frames/mferTest/api/images/justGettingStarted`;
-  const imageUrl4 = `${NEXT_PUBLIC_URL}/frames/mferTest/api/images/gmfer`;
+  const imageUrl = `${NEXT_PUBLIC_URL}/frames/mferTest/api/images/alwaysHaveBeen`;
 
   if (isValid) {
     accountAddress = message.interactor.verified_accounts[0];
   }
 
-  if (message?.input) {
-    text = message.input;
-  }
-
-  if (message?.button === 1) {
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-        ],
-        image: {
-          src: imageUrl1,
-        },
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-  }
-
-  if (message?.button === 2) {
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-        ],
-        image: {
-          src: imageUrl2,
-        },
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-  }
-
-  if (message?.button === 3) {
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-        ],
-        image: {
-          src: imageUrl3,
-        },
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-  }
-
-  if (message?.button === 4) {
-    return new NextResponse(
-      getFrameHtmlResponse({
-        buttons: [
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-          {
-            label: 'Yes',
-          },
-        ],
-        image: {
-          src: imageUrl4,
-        },
-        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-      }),
-    );
-  }
-
   return new NextResponse(
     getFrameHtmlResponse({
-      buttons: [
-        {
-          label: 'Yes',
-        },
-        {
-          label: 'Yes',
-        },
-        {
-          label: 'Yes',
-        },
-        {
-          label: 'Yes',
-        },
-      ],
       image: {
-        src: imageUrl1,
+        src: imageUrl,
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
     }),
