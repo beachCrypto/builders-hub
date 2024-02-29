7ec1ce402f4f82fe9a33fc12d55cdfbd181c0dcc
Beach Crypto <beachcrypto1@gmail.com>
2024-02-28 18:22:13 -0800

    Trying a default value

--- app/frames/mferMagazine/api/frame/1/route.ts
+++ app/frames/mferMagazine/api/frame/1/route.ts
@@ -6,10 +6,14 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
   let text: string | undefined = '';
 
+  let messageNum: number | undefined = 0;
+
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
   const imageUrl = `${NEXT_PUBLIC_URL}/frames/mferMagazine/api/images/1`;
+
+  messageNum = message?.button;
 
   if (isValid) {
     accountAddress = message.interactor.verified_accounts[0];
@@ -35,7 +39,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         src: imageUrl,
         aspectRatio: '1:1',
       },
-      postUrl: `${NEXT_PUBLIC_URL}/api/frame/${message?.button}`,
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame/messageNum`,
     }),
   );
 }
--- app/frames/mferMagazine/api/frame/2/route.ts
+++ app/frames/mferMagazine/api/frame/2/route.ts
@@ -6,8 +6,12 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
   let text: string | undefined = '';
 
+  let messageNum: number | undefined = 0;
+
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+  messageNum = message?.button;
 
   const imageUrl = `${NEXT_PUBLIC_URL}/frames/mferMagazine/api/images/2`;
 
@@ -35,7 +39,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         src: imageUrl,
         aspectRatio: '1:1',
       },
-      postUrl: `${NEXT_PUBLIC_URL}/api/frame/${message?.button}`,
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame/messageNum`,
     }),
   );
 }
