2041819e91c0e78e5027e82c5085b970695c1763
Beach Crypto <beachcrypto1@gmail.com>
2024-03-03 20:17:24 -0800

    Requesting data from API

--- app/frames/theBasedLPSudoSwap/api/images/1/route.tsx
+++ app/frames/theBasedLPSudoSwap/api/images/1/route.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from 'next/og';
+// import Web3 from 'web3';
 
 export const runtime = 'edge';
 
@@ -6,6 +7,44 @@ export async function GET() {
   const fontData = await fetch(
     new URL('../../../../../../public/theBasedLP/fonts/VT323/VT323-Regular.ttf', import.meta.url),
   ).then((res) => res.arrayBuffer());
+
+  async function getData() {
+    try {
+      const headers = {
+        'content-type': 'application/json',
+        Authorization: process.env.DEFINED_API_KEY || '',
+      };
+      const requestBody = {
+        query: `{
+        getNftPool(
+          address: "0x6f3714d92e5ac5fb6c9611ea5e860920075b1a77"
+          networkId: 8453
+        ) {
+          collectionAddress
+          exchangeAddress
+          floorT
+          offerT
+        }
+      }`,
+      };
+      const options = {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(requestBody),
+      };
+      const res = await (await fetch('https://graph.defined.fi/graphql', options)).json();
+      console.log('RESPONSE FROM FETCH REQUEST', res?.data);
+      return res?.data;
+    } catch (err) {
+      console.log('ERROR DURING FETCH REQUEST', err);
+    }
+  }
+
+  const theBasedLPData = await getData();
+
+  // const floorT = Web3.utils.fromWei(theBasedLPData.getNftPool.floorT, 'ether').slice(0, 6);
+
+  const floorT = theBasedLPData.getNftPool.floorT;
 
   return new ImageResponse(
     (
@@ -23,7 +62,8 @@ export async function GET() {
           fontFamily: '"SartoshiScript"',
         }}
       >
-        This is a test
+        Updated
+        <p>Selling at: {floorT} ETH</p>
       </div>
     ),
     {
